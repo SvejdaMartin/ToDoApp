add9e121cb47f70dfa23936453e56d3b20cd21c6
svejdam <svejdam13@gmail.com>
2025-01-09 19:05:40 +0100

    add comments

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -167,7 +167,7 @@ function loadTasks() {
                 taskList.appendChild(taskItem);
             });
         })
-        .catch(error => console.error('Chyba při načítání úkolů:', error));
+        .catch(error => console.error('Error: tasks not updated', error));
 }
 // Reload tasks (refresh the page)
 window.onload = () => filterTasks('ALL');
