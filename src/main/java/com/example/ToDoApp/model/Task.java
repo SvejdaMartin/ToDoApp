65d9c3c6b48f401c3c3c9d838ae2117e4641a7ad
svejdam <svejdam13@gmail.com>
2025-01-12 10:36:56 +0100

    add comments

--- src/main/java/com/example/ToDoApp/model/Task.java
+++ src/main/java/com/example/ToDoApp/model/Task.java
@@ -18,35 +18,44 @@ public class Task {
 
     private boolean completed;
 
-    // Getters a setters
+    // Getters and setters
 
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public Priority getPriority() {
         return priority;
     }
+
     public void setPriority(Priority priority) {
         this.priority = priority;
     }
+
     public boolean isCompleted() {
         return completed;
     }
+
     public void setCompleted(boolean completed) {
         this.completed = completed;
     }
